fdad09e0f9853da482133b40e526954f48b28247
msyoder <yoderm01@gmail.com>
2015-01-04 19:08:37 -0500

    Add make.js depedancies to package.json. Improved build manifest with platform, hostname, and environment variables.

--- make.js
+++ make.js
@@ -7,6 +7,7 @@
 var program = require('commander');
 // shelljs is great! use it!
 var _shell = require('shelljs');
+var _os = require('os');
 // var _fs = require('fs');
 var _pkg = require('./package.json');
 
@@ -17,6 +18,7 @@ var _pkg = require('./package.json');
 
 var DIST_PATH = _pkg.build_dir;
 var SRC_PATH = _pkg.src_dir;
+var build_manifest = undefined;
 var _props = undefined;
 var _validCmd = false;
 var _env = undefined;
@@ -101,75 +103,92 @@ if (!_validCmd) { program.help() };
 
 function initEnv(env){
 	if (!_props) { // Only initialize once.
+
 		_validCmd = true;
 		_env = env || 'localhost';
+
 		if (validEnvs.indexOf(_env) == -1) { // No such environment
 			console.log('\n  No such environment, %s.', _env);
 			program.help();
 		}; 
+
 		var prop_file_name = "./build_props." + _env + ".json";
 		_props = require(prop_file_name);
 
 		var build_date = new Date();
-		var build_manifest = '\n ' + program.name() + ' started with "' + process.argv + '"\n' +
+
+		build_manifest = '\n ' + program.name() + ' started with argv="' + process.argv + '"\n' +
 							'\n ' + build_date.toString() + '\n' +  // Includes time.
 							// build_date.toTimeString() + '\n' +
-							'\n Target environment=' + _env + '\n' +	
+							'\n Target environment=' + _env + '\n' +
+							'\n Build platform=' + process.platform + '\n' +
+							'\n Build host=' + _os.hostname() + '\n ' +
+							'\n CWD=' + process.cwd() + '\n' +
 	    					'\n Using package:' + '\n' +
 	    					JSON.stringify(_pkg, null, " ") + '\n' +
 	    					'\n Using properties:' + '\n' +
-	    					JSON.stringify(_props, null, " ");
+	    					JSON.stringify(_props, null, " ") +
+	    					'\n Using process environment:' + '\n' +
+	    					JSON.stringify(process.env, null, " ");
+
 	    if (! program.silent) {
  	       console.log(build_manifest);
     	}
-    	var build_manifest_file = DIST_PATH + '/' + "build_manifest";
 	}
 }
 
 function clean(){
+    if (! program.silent) {
+        console.log('\n Cleaning output files...');
+    }
 	_validCmd = true; // No initEnv here because environment is irrelevent to cleaning.
     _shell.rm('-rf', DIST_PATH);
-    if (! program.silent) {
-        console.log('\n Cleaned output files!');
-    }
 }
+
+
 function bake(env){
 	initEnv(env);
     clean();
+    if (! program.silent) {
+        console.log('\n Baking %s output files...', _env);
+    }
+
     // Prepare the distribution directory
     _shell.mkdir(DIST_PATH);
-    _shell.cp(SRC_FILES, DIST_PATH); // My build is to copy the distribution files to the DIST_PATH.
 
+    // Stamp it with the build manifest.
+    var build_manifest_file = DIST_PATH + '/' + "build_manifest.txt";
+    build_manifest.to(build_manifest_file);
+
+    _shell.cp(SRC_FILES, DIST_PATH); // My bake is to copy the distribution files to the DIST_PATH.
 
 	// var err = _shell.error();
 
 	// if (!err===null) { console.log(err) }
 
-    if (! program.silent) {
-        console.log('\n Baking %s output files!', _env);
-    }
     // use sed to perform global replace
 }
 
 function build(env){
+
 	initEnv(env);
 	bake(_env);
 
-    // concat and minify files here or do anything that modifies the baked dist files for the target environment.
-
 	if (! program.silent) {
-        console.log('\n Building %s!', _env);
+        console.log('\n Building %s...', _env);
     }
 	
+    // concat and minify files here or do anything that modifies the baked dist files for the target environment.
+
 }
 
 function install(env) {
 	initEnv(env);
 	build(_env);
-	// Execute the steps to depoy the distribution to the selected run-time environment.
     if (! program.silent) {
-        console.log('\n Installing %s!', _env);
+        console.log('\n Installing %s...', _env);
     }
+	// Execute the steps to depoy the distribution to the selected run-time environment.
 }
 
 /*
