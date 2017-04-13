8d85ab0d74d9d5ae279c257b8cb37eab19600393
Michael Yoder <yodercode@gmail.com>
2017-04-13 00:15:00 -0400

    First cut @ ziet. Discovered the proxy change in Node v 6.

--- make.js
+++ make.js
@@ -36,7 +36,7 @@ var _env = undefined;
 */
 
 // Names of valid environments with definition property files defined
-var validEnvs = ['localnet','localhost','jitsu','modulus', '127'];
+var validEnvs = ['localnet','localhost','jitsu','modulus', '127', 'zeit'];
 
 // Client source and lib files to copy to static directory
 var SRC_FILES_NAMES = ['TodoServices.js', 'todo.html', 'todo.js', 'list.html', 'index.html', 'todo.css', 'welcome.html'];
